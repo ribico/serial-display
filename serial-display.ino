9d1b105548afa6495176f82b809e1b7587424a7d
Gabriele Ribichini <gabriele@ribichini.net>
2015-07-02 23:10:32 +0200

    added display wiring info

--- serial-display.ino
+++ serial-display.ino
@@ -1,3 +1,17 @@
+/*
+The Cuicuit:
+ LCD RS pin to D12
+ LCD Enable pin to D11
+ LCD D4 pin to D5
+ LCD D5 pin to D4
+ LCD D6 pin to D3
+ LCD D7 pin to D2
+ LCD Vee tied to a pot to control brightness
+ LCD Vss and R/W tied to ground
+ LCD Vcc to +5V
+
+ */
+
 void setup() {
   // initialize serial:
   Serial.begin(9600);
